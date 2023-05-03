18745e93a9636128e377e691c2ab814fd1053955
Igor Balashov <wowzawow@list.ru>
2023-05-04 01:43:57 +0300

    Добавьте файлы проекта.

---
+++ Form1.Designer.cs
@@ -0,0 +1,132 @@
+﻿namespace IntSysLaba4V1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(108, 284);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 51);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Хорошо";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click_1);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(481, 284);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(118, 51);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Нет";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click_1);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(111, 71);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(488, 120);
+            this.label1.TabIndex = 2;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(271, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(259, 25);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Диагностика покраски";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button3.Location = new System.Drawing.Point(232, 284);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(243, 51);
+            this.button3.TabIndex = 6;
+            this.button3.Text = "Диагностировать заного";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Visible = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.richTextBox1.Location = new System.Drawing.Point(27, 426);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(448, 201);
+            this.richTextBox1.TabIndex = 7;
+            this.richTextBox1.Text = "Факторы уверенности: \n";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 639);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,280 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Reflection.Emit;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace IntSysLaba4V1
+{
+    public partial class Form1 : Form
+    {
+        int counterPopitok = 0;
+        List<int> HistoryCFs = new List<int>();
+        public void ButtonsVisable(bool bl)
+        {
+            button1.Visible = bl;
+            button2.Visible = bl;
+        }
+        
+        // Экземпляры класса, представляющие из себя все вопросы и ответы
+        public DNode QDefects = new DNode("Появились ли дефекты на окрашенной поверхности ?", 98);
+        public DNode QPytna = new DNode("Эти дефекты-пятна?", 80);
+        public DNode QPolosy = new DNode("Эти дефекты-полосы?", 40);
+        public DNode QPyziri = new DNode("Эти дефекты-пузыри?", 50); // добавленный вопрос по варианту 2А
+        public DNode QZhirPytna = new DNode("Это жирные пятна?",60);
+        public DNode QPoverhShtyk = new DNode("Окрашенная поверхность является штукатуркой?",35);
+        public DNode QZhelezobeton = new DNode("Окрашенная поверхность являетсяжелезобетонной?",45);
+        public DNode QRzhavchina = new DNode("Это желтые ржавые пятна?", 65);
+        public DNode AOtdih = new DNode("Если нет дефектов то ничего делать не нужно. \nВот что вам следует сделать в данной ситуации:" +
+            " \nПойдите отдохните, у вас все в порядке.",20);
+        public DNode ANotZhelezobeton = new DNode("Причина появления этих пятен мне неизвестна. \nВот что вам следует сделать в данной ситуации:" +
+            " \nПопробуйте обратиться к более опытному специалисту.",25);
+        public DNode AZhelezobeton = new DNode("Если жирные пятна на железобетоне, \nто эта проблема вполне разрешима." +
+            " \nВот что вам следует сделать в данной ситуации: \nОчистить поверхность от слоя краски со шпаклевкой. \nПромыть 5%-м раствором кальцевой соды," +
+            " \nнейтрализовать поверхность 5%-м \nраствором соляной кислоты и вновь окрасить", 75);
+        public DNode APoverhShtyk = new DNode("Если жирные пятна на штукатурке, \nто эта проблема вполне разрешима. \nВот что вам следует сделать в данной ситуации:" +
+            " \nВырубить штукатурку на участке пятна, \nвновь оштукатурить и окрасить.", 55);
+        public DNode ANotRzavchina = new DNode("Если появились какие-то другие пятна, \nто ничего путного посоветовать вам я не могу." +
+            " \nВот что вам следует сделать в данной ситуации: \nПопробуйте обратиться к более опытному специалисту.",28);
+        public DNode ARzhavchina = new DNode("Если ржавые пятна на штукатурке, \nто эта проблема вполне разрешима." +
+            " \nВот что вам следует сделать в данной ситуации: \nУдалить старый набел, \nи промыть 3%-м раствором соляной кислоты" +
+            " \nи если пятна не велики, отгрунтовать \nмеднокупоросной грунтовкой без мела, \nа при больших размерах щелочным или канифольным лаком.", 72);
+        public DNode APolosy = new DNode("Если полосы на окрашенной поверхности, \nто эта проблема вполне разрешима." +
+            " \nВот что вам следует сделать в данной ситуации: \nПромыть поверхность и окрасить.", 50);
+        public DNode ANotPyziri = new DNode("Если появились какие-то другие дефекты \nто ничего путного я вам посоветовать не могу." +
+            " \nВот что вам следует сделать в данной ситуации: \nПопробуйте обратиться к более опытному специалисту.", 15);
+        public DNode APyziri = new DNode("Если появились пузыри на окрашенной поверхности, \nто эта проблема вполне разрешими." +
+            " \nВот что вам следует сделать в данной ситуации: \nОчистить поверхность от краски, \nперемешивать краску до удаления в ней пузырей воздуха," +
+            " \nснова выполнить покраску.", 85);
+        public DNode CurrentNode = new DNode("");  //узел, который будем менять в зависимости от ответа
+        public DNode NotCurrentNode = new DNode(""); // тупиковый узел 
+        public DNode StartNode = new DNode("");
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        public void PishetZapominaet(DNode dNode) // несколько действий вынесено в этот отдельный метод для сокращения кода
+        {
+            if (CurrentNode.Left == null)                      // если вопросов больше не будет, то кнопки убираются через if.
+            {
+                counterPopitok++;
+                this.ButtonsVisable(false);
+                richTextBox1.Text += "\nНа " + Convert.ToString(counterPopitok) + " попытке результаты : \n";
+                richTextBox1.Text += "Вычисление в пределах правила 5 методом : " + Convert.ToString(CurrentNode.MetodCF5(CurrentNode)) + "%\n";
+                richTextBox1.Text += "Вычисление в пределах правила 6 методом : " + Convert.ToString(CurrentNode.MetodCF6(CurrentNode)) + "%\n";
+                richTextBox1.Text += "Вычисление в пределах правила 7 методом : " + Convert.ToString(CurrentNode.MetodCF7(CurrentNode)) + "%\n";
+                richTextBox1.Text += "Вычисление в пределах правила 8 методом : " + Convert.ToString(CurrentNode.MetodCF8(CurrentNode)) + "%\n";
+
+                HistoryCFs.Add(CurrentNode.MetodCF5(CurrentNode));
+                HistoryCFs.Add(CurrentNode.MetodCF6(CurrentNode));
+                HistoryCFs.Add(CurrentNode.MetodCF7(CurrentNode));
+                HistoryCFs.Add(CurrentNode.MetodCF8(CurrentNode));
+
+                button3.Visible = true;
+                if (counterPopitok > 1)
+                {
+                    richTextBox1.Text += "\nВычисление по правилам после " + Convert.ToString(counterPopitok) + " попытки : "
+                        + Convert.ToString(CurrentNode.MaxZnach(HistoryCFs)) + "\n";
+                }
+            }
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            CurrentNode = CurrentNode.Yes(CurrentNode, label1);  // Метод принимает currentnode - сдвигает его влево и выводит текст на экран, 
+            button1.Text = "Да";                                // currentnode обновляется. label принимается для обновления на нем текста.
+            PishetZapominaet(CurrentNode);
+        }
+        private void button2_Click_1(object sender, EventArgs e)
+        {
+            CurrentNode = CurrentNode.No(CurrentNode, label1);
+            if (CurrentNode.Data == "")
+            {
+                this.Close();
+            }
+            PishetZapominaet(CurrentNode);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //Устанавливается порядок узлов в дереве.
+            StartNode.Mesto(null, QDefects, NotCurrentNode);
+            CurrentNode.Mesto(null, QDefects, NotCurrentNode);
+            QDefects.Mesto(QDefects, QPytna, AOtdih);
+            QPytna.Mesto(QPytna, QZhirPytna, QPolosy);
+            QZhirPytna.Mesto(QZhirPytna, QPoverhShtyk, QRzhavchina);
+            QRzhavchina.Mesto(QRzhavchina, ARzhavchina, ANotRzavchina);
+            QPoverhShtyk.Mesto(QPoverhShtyk, APoverhShtyk, QZhelezobeton);
+            QZhelezobeton.Mesto(QZhelezobeton, AZhelezobeton, ANotZhelezobeton);
+            QPolosy.Mesto(QPolosy, APolosy, QPyziri);
+            QPyziri.Mesto(QPyziri, APyziri, ANotPyziri);
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            CurrentNode = StartNode;
+            label1.Text = "После окраски поверхности водными красками могут\r\nпроявиться дефекты.\r" +
+                "\nМы постараемся дать вам совет по их устранинию и\r\nпричинам появления.\r\nНо для этого вы должны предоставить мне всю информацию.\r\n\tитак начнем... ";
+            button3.Visible = false;
+            this.ButtonsVisable(true);
+        }
+    }
+    public class DNode
+    {
+        public string Data { get; set; }
+        public DNode Left { get; set; }
+        public DNode Right { get; set; }
+        public DNode Parent { get; set; }
+        public int CF { get; set; }
+        public DNode(string data, int cF)
+        {
+            Data = data;
+            CF = cF;
+        }
+        public DNode(string data)
+        {
+            Data = data;
+        }
+        public void Mesto(DNode parent, DNode left, DNode right)
+        {
+            Left = left;
+            Right = right;
+            Left.Parent = parent;
+            Right.Parent = parent;
+        }
+        public DNode Yes(DNode CurrentNode, System.Windows.Forms.Label label1)
+        {
+            if (CurrentNode.Left != null)
+            {
+                CurrentNode = CurrentNode.Left;
+                label1.Text = CurrentNode.Data;
+                return CurrentNode;
+            }
+            else
+            {
+                return CurrentNode;
+            }
+        }
+        public DNode No(DNode CurrentNode, System.Windows.Forms.Label label1)
+        {
+            if (CurrentNode.Right != null)
+            {
+                CurrentNode = CurrentNode.Right;
+                label1.Text = CurrentNode.Data;
+                return CurrentNode;
+            }
+            else
+            {
+                return CurrentNode;
+            }
+        }
+        public int YmnozitIPodelitNa100(List<int> ints)
+        {
+            double multiplicate = 1;
+            int counter = 0;
+            List<Double> doubles = new List<Double>();
+            foreach (int i in ints)
+            {
+                doubles.Add(Convert.ToDouble(i));
+            }
+            foreach (double d in doubles)
+            {
+                multiplicate *= d;
+                counter++;
+            }
+            for (int i = 0; i < counter - 1; i++)  // делим на 100 каждую пару, -1 - чтобы при 2х числах не делить 2 раза на 100
+            {
+                multiplicate /= 100;
+            }
+            return Convert.ToInt32(multiplicate);
+        }
+        public int MinZnach(List<int> ints)
+        {
+
+            int dlinna = ints.Count;
+            if (dlinna > 1)
+            {
+                int minValue = ints[0];
+                for (int i = 0; i < dlinna; i++)
+                {
+                    if (ints[i] < minValue)
+                    {
+                        minValue = ints[i];
+                    }
+                }
+                return minValue;
+            }
+            else if (dlinna == 1)
+            {
+                return ints.First();
+            }
+            else
+            {
+                throw new Exception("Никого нет");
+            }
+        }
+        public int MaxZnach(List<int> ints)
+        {
+            int dlinna = ints.Count;
+            if (dlinna > 1)
+            {
+                int maxValue = ints[0];
+                for (int i = 0; i < dlinna; i++)
+                {
+                    if (ints[i] > maxValue)
+                    {
+                        maxValue = ints[i];
+                    }
+                }
+                return maxValue;
+            }
+            else if (dlinna == 1)
+            {
+                return ints.First();
+            }
+            else
+            {
+                throw new Exception("Никого нет");
+            }
+        }
+        public List<int> CountCFParents(DNode dNode) // вернет лист всех коэфициентов нода и родителей
+        {
+            int counterParents = 0;
+            DNode Clone = dNode;
+            while (dNode.Parent != null)
+            {
+                dNode = dNode.Parent;
+                counterParents++;
+            }
+            dNode = Clone;
+            List<int> CFParentsList = new List<int>();
+            for (int i = 0; i < counterParents + 1; i++) // закидываем в лист все значения факторов уверенности родителей и потомка +1 из-за потомка
+            {
+                CFParentsList.Add(dNode.CF);
+                dNode = dNode.Parent;
+            }
+            return CFParentsList;
+        }
+
+        public int MetodCF5(DNode dNode)
+        {
+            return YmnozitIPodelitNa100(CountCFParents(dNode));
+        }
+        public int MetodCF6(DNode dNode)
+        {
+            return MinZnach(CountCFParents(dNode));
+        }
+        public int MetodCF7(DNode dNode)
+        {
+            return ((MinZnach(CountCFParents(dNode)) + YmnozitIPodelitNa100(CountCFParents(dNode))) / 2);
+        }
+        public int MetodCF8(DNode dNode)
+        {
+            return ((YmnozitIPodelitNa100(CountCFParents(dNode))) * (200 - MaxZnach(CountCFParents(dNode))) / 100);
+        }
+    }
+}
